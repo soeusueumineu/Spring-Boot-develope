da5d565e0f03404d7f425b2b54d793da5db6a746
song sungmin <137161603+soeusueumineu@users.noreply.github.com>
2024-11-04 17:51:05 +0900

    Add files via upload

---
+++ chapter5/src/main/java/me/songsungmin/springbootdeveloper/TestService.java
@@ -0,0 +1,15 @@
+package me.songsungmin.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+    @Autowired
+    MemberRepository memberRepository; 	 // 1 빈 주입
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();	// 2 멤버 목록 얻기
+    }
+}
